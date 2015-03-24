2d7f9c1e18356d10bde0047dfed3146bcdb8c0ae
Arjan Molenaar <gaphor@gmail.com>
2015-03-24 15:55:17 +0100

    Remove more compile time dependencies.

--- src/main/java/com/xebialabs/xltest/ci/server/XLTestServerFactory.java
+++ src/main/java/com/xebialabs/xltest/ci/server/XLTestServerFactory.java
@@ -22,36 +22,9 @@
  */
 package com.xebialabs.xltest.ci.server;
 
-import java.util.List;
-import com.google.common.base.Joiner;
-import com.google.common.collect.Lists;
-import com.google.common.reflect.Reflection;
-
-
 public class XLTestServerFactory {
-    public boolean validConnection(String serverUrl, String proxyUrl, String username, String password) throws IllegalStateException {
-        newInstance(serverUrl, proxyUrl, username, password).newCommunicator();  //throws IllegalStateException on failure.
-        return true;
-    }
-
 
     public static XLTestServer newInstance(String serverUrl, String proxyUrl, String username, String password) {
-        XLTestServerImpl server = new XLTestServerImpl(serverUrl, proxyUrl, username, password);
-        return Reflection.newProxy(XLTestServer.class, new PluginFirstClassloaderInvocationHandler(server));
-    }
-
-
-    public static String getNameFromId(String id) {
-        String[] nameParts = id.split("/");
-        return nameParts[nameParts.length - 1];
-    }
-
-    public static String getParentId(String id) {
-        String[] nameParts = id.split("/");
-        List<String> list = Lists.newArrayList(nameParts);
-        if (list.size() > 1) {
-            list.remove(nameParts.length - 1);
-        }
-        return Joiner.on("/").join(list);
+        return new XLTestServerImpl(serverUrl, proxyUrl, username, password);
     }
 }
