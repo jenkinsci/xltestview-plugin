500cd1dd4f8042865549c8fb7f5840defbbebc89
Ric Klaren <rklaren@xebialabs.com>
2015-04-23 15:24:54 +0200

    Clean up unused stuff

--- src/main/java/com/xebialabs/xltest/ci/NameValuePair.java
+++ src/main/java/com/xebialabs/xltest/ci/NameValuePair.java
@@ -1,21 +1,21 @@
 /**
  * Copyright (c) 2014-2015, XebiaLabs B.V., All rights reserved.
- *
+ * <p/>
  * The XL Test plugin for Jenkins is licensed under the terms of the GPLv2
  * <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most XebiaLabs
  * Libraries. There are special exceptions to the terms and conditions of the
  * GPLv2 as it is applied to this software, see the FLOSS License Exception
  * <https://github.com/jenkinsci/xltest-plugin/blob/master/LICENSE>.
- *
+ * <p/>
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation; version 2 of the License.
- *
+ * <p/>
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
  * more details.
- *
+ * <p/>
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
@@ -23,21 +23,11 @@
 package com.xebialabs.xltest.ci;
 
 
-import java.util.Collection;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.Transformer;
-import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.QueryParameter;
-
-import com.xebialabs.xltest.ci.util.ListBoxModels;
-import com.xebialabs.xltest.ci.util.TemplateVariable;
-
 import hudson.Extension;
-import hudson.RelativePath;
 import hudson.model.AbstractDescribableImpl;
 import hudson.model.Descriptor;
-import hudson.util.ListBoxModel;
 import jenkins.model.Jenkins;
+import org.kohsuke.stapler.DataBoundConstructor;
 
 public class NameValuePair extends AbstractDescribableImpl<NameValuePair> {
 
